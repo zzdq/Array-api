da13a5f6d81a98c1aca82b4ec343aa4ad79ee568
zzdq <32027442+zzdq@users.noreply.github.com>
2019-10-12 14:41:55 +0800

    数组的copyWithin和every方法

--- array.of.js
+++ array.of.js
@@ -3,7 +3,7 @@
  * @item1, item2
 */
 var result = Array.of(4, 2);
-console.log(result); // result [4， 2]
+// console.log(result); // result [4， 2]
 
 // 兼容性问题
 if (!Array.of) {
@@ -11,3 +11,20 @@ if (!Array.of) {
     return Array.prototype.slice.call(arguments);
   };
 }
+
+// copyWithin 复制目标数组中相对位置的元素 target start end
+/*
+  *@target 目标数组
+  *@start 开始位置 正数 从开头开始 从0 算起 负数则从尾部算起 不包括开始位置的元素
+  *@target 拷贝位置 
+*/
+var target = [1, 2, 3, 5, 7];
+var arr1 = target.copyWithin(target, 2, 4);
+// console.log(arr1);  // [3, 5, 3, 5, 7]
+
+// every 所有元素都满足条件 返回true
+var entires1 = [1, 2, 3, 4];
+var every1 = entires1.every((item) => {
+  return item >= 1;
+}, this);
+console.log(every1);
